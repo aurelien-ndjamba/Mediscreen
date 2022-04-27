0d5a8e6c816be1d29ee64d89fe0f95c8ac192463
andjamba <aurelien.ndjamba@gmail.com>
2022-04-27 16:21:04 +0200

    InfoPatientApplicationTests de l'API infoPatient OK

---
+++ infoPatient/src/test/java/com/mediscreen/infoPatient/InfoPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.infoPatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
