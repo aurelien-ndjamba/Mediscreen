b819f682fdf4953580304546d4030c81b25cd2cc
andjamba <aurelien.ndjamba@gmail.com>
2022-04-27 16:16:37 +0200

    Main de l'API infoPatient OK

---
+++ infoPatient/src/main/java/com/mediscreen/infoPatient/InfoPatientApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.infoPatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoPatientApplication.class, args);
+	}  
+  
+}
